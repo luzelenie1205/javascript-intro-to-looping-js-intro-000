7540c596ca52f6250a12beb1b18f2bb5484545d2
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-14 03:59:29 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -28,7 +28,7 @@ function doWhileLoop(num) {
   do {
     console.log(i, "I run once regardless.");
   }
-  while (incrementVariable() <= num;
+  while (incrementVariable() <= num);
 
 doWhileLoop(0);
 }
